c3cd0e9df08131509bc45a71aeda487965e9472f
mithunraj <mithun.raj159@gmail.com>
2020-09-19 14:39:38 +0530

    Initial Commit

---
+++ TipsCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipsCalculator
+//
+//  Created by Mithun Raj on 19/09/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
